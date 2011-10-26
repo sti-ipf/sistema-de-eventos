c976baf917e38455d90297efd1cbae51d317e2f9
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-10-26 15:21:41 -0200

    adicionado total por tipo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @total_registrations = Registration.count
+    @total_presencial = Registration.count(:conditions => "participation_type = 0")
+    @total_remote = Registration.count(:conditions => "participation_type = 1")
     @total_papers = Paper.count
   end
   
