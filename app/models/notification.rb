b3a428ada7245ce56349bc0ca54b79e465c70be1
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-10-31 12:38:24 -0200

    arrumado envio de email

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,7 +7,7 @@ class Notification < ActionMailer::Base
     @activity = activity.name if !activity.nil?
     puts "Enviando notificacao para o email: #{@registration.email}"
     subject = "Confirmação da Inscrição no evento Paulo Freire 90 anos"
-    mail(:from => 'eiesistema@paulofreire.org', :to => @registration.email, :subject => subject)
+    mail(:from => '90anos@paulofreire.org', :to => @registration.email, :subject => subject)
     ActiveRecord::Base.connection.execute "UPDATE registrations set notificator_status = 1 WHERE id = #{@registration.id}"
     puts "Notificao enviada para o email: #{@registration.email}"
   end
@@ -15,10 +15,9 @@ class Notification < ActionMailer::Base
   def link_to_update_new_data(registration_id)
     @registration = Registration.find(registration_id)
     puts "Enviando notificacao para o email: #{@registration.email}"
-    subject = "Confirmação da Inscrição no evento Paulo Freire 90 anos"
-    mail(:from => 'eiesistema@paulofreire.org', :to => 'ffc.fabricio@gmail.com', :subject => subject)
+    subject = "IMPORTANTE: Retificação no cadastro de participação"
+    mail(:from => '90anos@paulofreire.org', :to => @registration.email, :bcc => 'ffc.fabricio@gmail.com', :subject => subject)
     ActiveRecord::Base.connection.execute "UPDATE registrations set notificator_status = 2 WHERE id = #{@registration.id}"
-    
   end
 
 end
