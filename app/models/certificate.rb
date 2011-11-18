681e48872218e10a5117cbb3453d0118563b5a0f
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-11-18 17:29:37 -0200

    atualizada geração do certificado

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -3,22 +3,20 @@ class Certificate < ActiveRecord::Base
   belongs_to :registration
 
   def self.generate_all
-    registrations = Registration.all
+    registrations = Registration.all(:conditions => "name IN (select name from participants)")
     registrations.each do |r|
       file = fill_with_data(create_file('90anos'), r)
       name = r.name.gsub(/[^a-zA-Z0-9 ]/,"").downcase
       file_path = "certificates/#{r.id}_#{name}.pdf"
       file.render_file("#{RAILS_ROOT}/public/#{file_path}")
       Certificate.create(:registration_id => r.id, :file_path => file_path)
-      break
     end
   end
 
 private
 
   def self.fill_with_data(file, registration)
-    puts registration.name
-    file.draw_text registration.name, :at => [170,280], :size => 14
+    file.draw_text registration.name, :at => [825,1280], :size => 60
     file
   end
 
